0607017f20a4b99631897d32e143821946156b0e
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-01-12 22:54:34 +0700

    #code

--- speech_api/routes.py
+++ speech_api/routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, HTTPException, status
-
+from fastapi import APIRouter, HTTPException, status, UploadFile
+from speech_api.services import correcting_service
+""" modules """
 router = APIRouter()
 
 @router.get('/', status_code=status.HTTP_200_OK)
@@ -7,10 +8,14 @@ def index():
     return {"message": "Hello word"}
 
 @router.post('/danangvsr/vmd', status_code=status.HTTP_200_OK)
-def correction_route():
-    """ input of correction route is "wav" file
-    the service responsible for dealing with wav file
-    :return the output contain phonemes each one will be associated with a tag
-    that labeled T(true), F(false)
-    """
+def correction_route(file: UploadFile):
+    """ input of correction route is "wav" file and text """
+    # text = request.text
     return
+
+@router.post('/test/upload', status_code=status.HTTP_200_OK)
+def test_route(file: UploadFile):
+    size = file.size
+    name = file.filename
+    result = {size, name}
+    return {"Response": result}
--- speech_api/services.py
+++ speech_api/services.py
@@ -1,3 +1,8 @@
 # service class
-def correcting_service(file):
+def correcting_service(media, text):
+    """ the service responsible for dealing with wav file
+    :return the output contain phonemes each one will be associated with a tag
+    that labeled T(true), F(false)
+    """
+
     return
