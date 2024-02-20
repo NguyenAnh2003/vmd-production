00c10c5146d9d1ff7fe7c6c4468b784ce885f89e
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-20 20:06:33 +0700

    #update code

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from setup.setup_app import setup_app
 import uvicorn
+from core.mylogger import setup_logger
+
+# setup logger
+_logger = setup_logger("./core/logs/app_run.log")
 
 """ setup app and run """
 app = setup_app() #
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001, log_level="info", reload=True,
-                reload_delay=0.5)
+    _logger.log(_logger.INFO, "App running")
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, log_level="info", reload=True,
+                reload_delay=0.5, use_colors=True)
