19d37b62ca72ee679a89c866796bbf9ecc51c876
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-02-28 13:25:14 +0700

    #code

--- app/view/interface2.py
+++ app/view/interface2.py
@@ -1,28 +1,19 @@
 import streamlit as st
-import sounddevice as sd
-import numpy as np
-from scipy.io.wavfile import write
 
-def record_audio(duration, fs):
-    st.text("Recording...")
-    audio_data = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
-    sd.wait()
-    st.text("Recording done.")
-    return audio_data.flatten()
+def colorize(value):
+    if value == 1:
+        return "color: green"
+    elif value == 0:
+        return "color: red"
+    else:
+        return ""
 
 def main():
-    st.title("Streamlit Audio Recorder")
-
-    duration = st.slider("Recording Duration (seconds):", 1, 10, 5)
-    fs = 44100  # You can adjust the sample rate if needed
-
-    if st.button("Record Audio"):
-        audio_data = record_audio(duration, fs)
-
-        # Save the recorded audio as a WAV file
-        write("recorded_audio.wav", fs, audio_data)
-
-        st.audio("recorded_audio.wav", format="audio/wav", start_time=0)
+    result = {"bao": 1, "vây": 0, "anh": 1, "bảy": 0}
+    result_html = ""
+    for key, value in result.items():
+        result_html += f"<p style='{colorize(value)}; margin-left: 5px; font-size: 25px'>{key}</p>"
+    st.markdown(f"<div style='display: flex; flex-direction: row; gap: 0;'>{result_html}</div>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
