56d60f757392d57ec2a14acd49d4efea1c045eb0
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-03 11:35:50 +0700

    #code

--- app/view/interface.py
+++ app/view/interface.py
@@ -56,6 +56,8 @@ def main():
             data_package = {"text_target": text, "username": username, "country": country, "age": age}
             response = requests.post("http://127.0.0.1:8000/danangvsr/vmd", files=file_data, data=data_package)
 
+            st.write(OUT_WAV_FILE, text, username, country, age)
+
             if response.status_code == 200:
                 # Parse the JSON response
                 result = response.json() # casts to JSON
