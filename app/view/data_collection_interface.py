16515ad704b499bda9fbe642ac9eccc521ed3f2a
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-03-05 21:47:51 +0700

    #code

--- app/view/data_collection_interface.py
+++ app/view/data_collection_interface.py
@@ -6,10 +6,20 @@ import soundfile as sf
 import requests
 import time
 from dotenv import load_dotenv
+import supabase
+from supabase import create_client, Client
+import os
 
+# init DB
+url: str = "https://cceebjjirmrvyhqecubk.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjZWViamppcm1ydnlocWVjdWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2NDMxMTMsImV4cCI6MjAyNTIxOTExM30.dh4WE15QV41Ch7GZlpNyELOa6ZZiapV9RsYHuHi6ZQ8"
+DB: Client = create_client(supabase_url=url, supabase_key=key)
+
+print(f"Supabase: {DB}")
 
 # demo app using streamlit integrating model prediction -> return mapped result
 # call api to save data recorded and call model api to predict
+
 def colorize(value):
     if value == 1:
         return "color: green"
@@ -21,7 +31,7 @@ def colorize(value):
 
 def main():
     # setup interface
-    st.title(""" Hello, we need your information to improve our service """)
+    st.title(""" Data collection """)
 
     text = st.text_input('Target text', '')
     st.write('The current test is', text)
@@ -30,12 +40,12 @@ def main():
     age = st.number_input("Your age", min_value=0)
 
     # Record audio using the audio_recorder function
-    audio_bytes = audio_recorder(text="", pause_threshold=1, sample_rate=41_000)
+    audio_bytes = audio_recorder(text="", pause_threshold=1, sample_rate=44_100)
 
     if audio_bytes:
         st.audio(audio_bytes, format="audio/wav")
 
-    if st.button("Compute"):
+    if st.button("Save data"):
         # Convert audio_bytes to a NumPy array
         audio_array = np.frombuffer(audio_bytes, dtype=np.int16)
 
@@ -48,37 +58,30 @@ def main():
             Alternative can use Cloudinary service
             """
             OUT_WAV_FILE = f"upload/recorded_audio{time.time()}.wav"  # define absolute path
-            sf.write(OUT_WAV_FILE, audio_array, 41000, 'PCM_16')
+            sf.write(OUT_WAV_FILE, audio_array, 44100, 'PCM_24')
 
-            # Use the recorded audio directly for the API request
-            file_data = {"file": (OUT_WAV_FILE,
-                                  io.BytesIO(audio_bytes), "audio/wav")}
+            # send audio file
+            bucket_res = DB.storage.from_("vmd-bucket").upload(file=OUT_WAV_FILE, path=f"{OUT_WAV_FILE}",
+                                                  file_options={"content-type": "audio/wav"})
+            print(f"Bucket: {bucket_res}")
+            # get audio_url
+            if bucket_res:
+                wav_url = DB.storage.from_("vmd-bucket").get_public_url(path=f"{OUT_WAV_FILE}")
+                print(f"Wav url: {wav_url}")
+                response = DB.table("vmd-data").insert({"audio_url": wav_url, "text_target": text,
+                                                        "username": username, "country": country,
+                                                        "age": age}).execute()
+                print(f"DB: {response}")
 
-            st.write(OUT_WAV_FILE, text, username, country, age)
-            st.write(file_data)
+                if response:
+                    st.write("THANKS = ]]]")
 
-            # packaging data form
-            data_package = {"text_target": text, "username": username, "country": country, "age": age}
-            response = requests.post("http://127.0.0.1:8000/danangvsr/vmd", files=file_data, data=data_package)
-
-            if response.status_code == 200:
-                # Parse the JSON response
-                result = response.json()  # casts to JSON
-
-                result_html = ""
-                for key, value in result.items():
-                    result_html += f"<p style='{colorize(value)}; margin-left: 5px; font-size: 25px'>{key}</p>"
-                st.markdown(f"<div style='display: flex; flex-direction: row; gap: 0;'>{result_html}</div>",
-                            unsafe_allow_html=True)
-
-                # Display the data in your Streamlit app
-                # st.write("API Response:", data, response.status_code) # including status code and data
-            else:
-                st.error(f"Failed to fetch data. Status code: {response.status_code}")
+                else:
+                    st.error(f"Failed to fetch data")
         else:
             st.warning("The audio data is empty.")
 
 
-# run interface ui `streamlit run app/view/interface.py`
+# run interface ui `streamlit run app/view/data_collection_interface.py`
 st.set_page_config(page_title="Mispronunciation detection")
 main()
