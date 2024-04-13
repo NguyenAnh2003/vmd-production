7c36e2d4925df97d6239d282929accf59aca2d9b
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-04-13 13:20:24 +0700

    #code

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ from setup.setup_app import setup_app
 import uvicorn
 from core.mylogger import setup_logger
 
-# setup logger
-_logger = setup_logger("./core/logs/app_run.log", location="main")
-_logger.getLogger("main")
-
 """ setup app and run """
 app = setup_app() #
 
 if __name__ == "__main__":
-    _logger.log(_logger.INFO, "App running")
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True,
                 reload_delay=0.5, use_colors=True)
