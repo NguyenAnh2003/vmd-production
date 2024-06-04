d844baee5426026900f014833402e2ec74bbc619
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-04 20:46:06 +0700

    #code

--- app/routes.py
+++ app/routes.py
@@ -22,11 +22,3 @@ def vmd_route(file: UploadFile = File(...), text_target: str = Form(...)):
     file_data = file.file # file content
     result = vmd_service(media=file_data, text=text_target)
     return result
-
-
-@router.post('/test/upload', status_code=status.HTTP_200_OK)
-async def test_route(file: UploadFile = File(...), text_target: str = Form(...)):
-    file_data = await file.read()
-    array, _ = torchaudio.load(io.BytesIO(file_data))
-    result = "success"
-    return {"Response": text_target, "Audio shape": array.shape}
