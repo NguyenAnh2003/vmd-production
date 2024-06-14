453c6116ec24d517812087fa35e40d1da1d7710b
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-14 20:46:50 +0700

    #code

--- core/mylogger.py
+++ core/mylogger.py
@@ -10,11 +10,3 @@ def setup_logger(path: str = "./logs/example.log", location: str = ""):
                         format=f'{location}: %(levelname)s: -- %(message)s --: %(asctime)s',
                         datefmt="%m/%d/%Y %I:%M:%S %p")
     return logging
-
-if __name__ == "__main__":
-    logger = setup_logger(location="mylogger")
-    logger.getLogger(__name__)
-    logger.log(logger.INFO, "INFO LOGGER")
-    logger.log(logger.DEBUG, "DEBUG LOGGER")
-    logger.log(logger.WARN, "WARN LOGGER")
-    logger.log(logger.CRITICAL, "CRITICAL LOGGER")
