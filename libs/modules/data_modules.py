0b6f016065c1bfea53c623d129f0a6bbb804fee2
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-18 12:14:32 +0700

    #code

--- libs/modules/data_modules.py
+++ libs/modules/data_modules.py
@@ -1,31 +1,45 @@
-def _split_charac(arg: str):
-    VIETNAMESE_CHARACTER_UPPER = (
-        "["
-        + "|".join(
-            [
-                "ABCDEFGHIJKLMNOPQRSTUVXYZ",
-                "ÀÁẢÃẠ",
-                "ĂẰẮẲẴẶ",
-                "ÂẦẤẨẪẬ",
-                "Đ",
-                "ÈÉẺẼẸ",
-                "ÊỀẾỂỄỆ",
-                "ÌÍỈĨỊ",
-                "ÒÓỎÕỌ",
-                "ÔỒỐỔỖỘ",
-                "ƠỜỚỞỠỢ",
-                "ÙÚỦŨỤ",
-                "ƯỪỨỬỮỰ",
-                "ỲÝỶỸỴ",
-            ]
-        )
-        + "]"
-    )
-    VIETNAMESE_CHARACTER_LOWER = VIETNAMESE_CHARACTER_UPPER.lower()
-    CHARACTER = (
-        "[" + VIETNAMESE_CHARACTER_UPPER[1:-1] + VIETNAMESE_CHARACTER_LOWER[1:-1] + "]"
-    )
+import re
 
+# Tách từ => phụ âm + nguyên âm -> List([PA], [NA]) | List(None, [NA])
+def _split_charac(arg: str):
+    VIETNAMESE_CHARACTER_UPPER = "[" + "|".join([
+        "ABCDEFGHIJKLMNOPQRSTUVXYZ",
+        "ÀÁẢÃẠ",
+        "ĂẰẮẲẴẶ",
+        "ÂẦẤẨẪẬ",
+        "Đ",
+        "ÈÉẺẼẸ",
+        "ÊỀẾỂỄỆ",
+        "ÌÍỈĨỊ",
+        "ÒÓỎÕỌ",
+        "ÔỒỐỔỖỘ",
+        "ƠỜỚỞỠỢ",
+        "ÙÚỦŨỤ",
+        "ƯỪỨỬỮỰ",
+        "ỲÝỶỸỴ"
+    ]) + "]"
+    VIETNAMESE_CHARACTER_LOWER = VIETNAMESE_CHARACTER_UPPER.lower()
+    CHARACTER = "[" + VIETNAMESE_CHARACTER_UPPER[1:-1] + VIETNAMESE_CHARACTER_LOWER[1:-1] + "]"
+
+    consonant = [
+        r"ngh", r"ch", r"gh", r"gi", r"kh", r"ng",
+        r"nh", r"th", r"tr", r"ph", r"qu",
+        r"b", r"c", r"d", r"đ", r"g", r"h",
+        r"k", r"l", r"m", r"n", r"p", r"q",
+        r"r", r"s", r"t", r"v", r"x",
+        f"(?={CHARACTER})\w*"
+    ]
+    consonant = r"(" + "|".join(consonant) + ")"
+
+    list_charact = []
+    for charac in re.split(consonant, arg, maxsplit=1):
+        if charac != '':
+            list_charact.append(charac)
+
+    if len(list_charact) == 1:
+        return None, list_charact[0]
+    else:
+        return list_charact[0], list_charact[1]
 
 # Lấy dữ liệu để so sánh
 def _get_vocab_from_file(file: str):
